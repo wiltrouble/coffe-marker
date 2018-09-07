b98febc92e240f2f24c95e4910bdb168adbc3bcf
Warchino <42896616+Warchino@users.noreply.github.com>
2018-09-06 20:44:41 -0400

    ControllerSuggestoin
    Controlador tentativo a cambios para unificar y crear el Coffee Maker

---
+++ DemoNWAR/src/coffeeMaker/CoffeeMaker.java
@@ -0,0 +1,47 @@
+package coffeeMaker;
+
+public class CoffeeMaker {
+	Boiler boiler = new Boiler();
+	Pot pot = new Pot();
+	Button button = new Button();
+	Light light = new Light();
+	SensorPot sensorPot = new SensorPot();
+	SensorBoiler sensorBoiler = new SensorBoiler();
+	SensorState state;
+	public void makeCoffee() {
+		//Es un ciclo infinito, pero tal vez se podria crear un flag para que cuando acabe el proceso cambie de estado
+		while (true) {
+			//Pregunta si cambia de estado el boton
+			if (button.state) {
+				
+				//Entra en funcionamiento el calentamiento del Boiler
+				if (sensorBoiler.getState()) {
+					boiler.startBrewing();
+					pot.heatPot();
+					
+					//Pregunta si se retira el pot de la zonaPot
+					if(sensorPot.getState()==state.EMPTY) {
+						boiler.stopBrewing();
+						pot.stopHeat();
+					}
+					
+					//Pregunta si ya no hay agua en el boiler, y si ya no hay acaba el proceso
+					if(sensorBoiler.getState()==state.EMPTY) {
+						boiler.stopBrewing();
+						light.finish();
+						pot.stopHeat();
+						button.off();
+						break;
+					}
+				}
+				
+			}
+			else {
+				//Enciende un LED de espera.
+				light.wait();
+			}
+			
+		}
+		
+	}
+}
