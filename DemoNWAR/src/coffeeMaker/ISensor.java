c8674274c502b2316ba3ddf19af4d3d593457fc4
Warchino <42896616+Warchino@users.noreply.github.com>
2018-09-11 11:17:22 -0400

    Erase UNUSED Eclipse

--- DemoNWAR/src/coffeeMaker/ISensor.java
+++
@@ -1,5 +0,0 @@
-package coffeeMaker;
-import coffeeMaker.enums.SensorStatePlate;
-public interface ISensor {
-	SensorStatePlate getState(int parameter);
-}
--- DemoNWAR/src/coffeeMaker/SensorStatePlate.java
+++
@@ -1,7 +0,0 @@
-package coffeeMaker;
-
-public enum SensorStatePlate {
-	EMPTY,
-	NOT_EMPTY,
-	WARMER_EMPTY;
-}
